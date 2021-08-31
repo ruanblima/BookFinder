3696aa657b525d54b6ac54a4986ea50082368da7
Ruan Lima <ruan.lima@dellead.com>
2021-08-30 22:10:49 -0300

    Setting up project and adding material icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'plugin:react-hooks/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['react', 'prettier', 'module-resolver'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js', '.tsx'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+      'babel-module': {},
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['./'],
+        alias: {
+          '~': './src',
+          '@src': './src',
+        },
+        extensions: ['.js', '.ios.js', '.android.js'],
+      },
+    ],
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'app',
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
