9ec7dbb789394579694ddaff9d29e68b39004dae
Ruan Lima <ruan.lima@dellead.com>
2021-08-30 22:11:21 -0300

    Config ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+
+import themes from '~/shared/themes';
 
 import Login from './views/Login';
 
-const App: React.FC = () => {
-  return <Login />;
-};
+const App: React.FC = () => (
+  <ThemeProvider theme={themes.light}>
+    <Login />
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/shared/utils/fontStyle/index.ts
@@ -0,0 +1,45 @@
+import { Dimensions } from 'react-native';
+import { moderateScale } from 'react-native-size-matters';
+
+const cache = {};
+
+function getFontSizeStyle(fontSize: any) {
+  if (cache[fontSize]) return cache[fontSize];
+
+  const style = { fontSize };
+  cache[fontSize] = style;
+
+  return style;
+}
+
+export function createStyle(fontSize: number, style?: any) {
+  const fontSizeStyle = getFontSizeStyle(fontSize);
+
+  if (style) {
+    return [style, fontSizeStyle];
+  }
+  return fontSizeStyle;
+}
+
+const vh = Dimensions.get('window').height / 100;
+const vw = Dimensions.get('window').width / 100;
+const vmin = Math.min(vh, vw);
+const vmax = Math.max(vh, vw);
+
+export function scaleFontSize(fontSize: number) {
+  const scaledFontSize = moderateScale(fontSize, 1 * (vmin / vmax));
+  return scaledFontSize;
+}
+
+export function sfs(fontSize: number | string) {
+  if (typeof fontSize === 'string') {
+    const [splittedFontSize] = fontSize.split('px');
+    if (splittedFontSize) {
+      return scaleFontSize(Number(splittedFontSize));
+    }
+    throw new Error("Input format is invalid! try '8px' or 8");
+  }
+  return scaleFontSize(fontSize);
+}
+
+export { s, vs, ms } from 'react-native-size-matters';
