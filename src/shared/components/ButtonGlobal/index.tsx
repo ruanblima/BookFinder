56db44740916bd2a92aa08a8abacad3da397cc59
Ruan Lima <ruan.lima@dellead.com>
2021-08-30 22:11:38 -0300

    Create ButtonGlobal

---
+++ src/shared/components/ButtonGlobal/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import * as S from './styles';
+
+interface ButtonGlobalProps {
+  title: string;
+  buttonType?: 'outline';
+  iconLeft?: string;
+  status?: 'primary' | 'error' | 'success' | 'warning';
+  disabled?: boolean;
+  action: () => void;
+}
+
+const ButtonGlobal: React.FC<ButtonGlobalProps> = ({
+  title,
+  status = 'primary',
+  buttonType,
+  disabled,
+  iconLeft,
+  action,
+}) => (
+  <S.Button
+    status={status}
+    disabled={disabled}
+    buttonType={buttonType}
+    onPress={() => action()}>
+    {iconLeft && (
+      <S.IconButton buttonType={buttonType} status={status} name={iconLeft} />
+    )}
+    <S.ButtonText buttonType={buttonType} status={status} title={title} />
+  </S.Button>
+);
+
+export default ButtonGlobal;
---
+++ src/shared/components/ButtonGlobal/styles.ts
@@ -0,0 +1,104 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/destructuring-assignment */
+import styled from 'styled-components/native';
+import { Pressable } from 'react-native';
+import { s, vs } from 'react-native-size-matters';
+
+import Text from '~/shared/components/Text';
+import Icon from '~/shared/components/Icon';
+
+interface StatusProps {
+  color:
+    | 'FEEDBACK_CORRECT'
+    | 'FEEDBACK_ERROR'
+    | 'FEEDBACK_WARNING'
+    | 'DELL_BLUE';
+}
+
+interface ButtonTypesProps {
+  success: StatusProps;
+  error: StatusProps;
+  warning: StatusProps;
+  primary: StatusProps;
+}
+
+const buttonTypes: ButtonTypesProps = {
+  success: {
+    color: 'FEEDBACK_CORRECT',
+  },
+  error: {
+    color: 'FEEDBACK_ERROR',
+  },
+  warning: {
+    color: 'FEEDBACK_WARNING',
+  },
+  primary: {
+    color: 'DELL_BLUE',
+  },
+};
+interface ButtonTextProps {
+  title: string;
+  buttonType?: string;
+  status: 'primary' | 'error' | 'success' | 'warning';
+}
+
+interface IconProps {
+  buttonType?: string;
+  status: 'primary' | 'error' | 'success' | 'warning';
+}
+
+interface ButtonProps {
+  buttonType?: string;
+  status: 'primary' | 'error' | 'success' | 'warning';
+}
+
+export const Button = styled(Pressable).attrs<ButtonProps>(
+  ({ buttonType, theme }) => ({
+    android_ripple: {
+      color:
+        buttonType === 'outline'
+          ? theme.Colors.BLUE_ACCENT
+          : theme.Colors.LIGHT_GRAY,
+      borderless: false,
+      radius: 18,
+    },
+  }),
+)<ButtonProps>`
+  background-color: ${({ theme, disabled, buttonType, status }) =>
+    disabled
+      ? theme.Colors.GRANITE
+      : buttonType === 'outline'
+      ? 'transparent'
+      : theme.Colors[buttonTypes[status].color]};
+  border-color: ${({ theme, status, disabled }) =>
+    disabled ? theme.Colors.GRANITE : theme.Colors[buttonTypes[status].color]};
+  padding: ${vs(8)}px ${s(8)}px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  border-radius: 2px;
+  border-width: 1px;
+`;
+
+export const ButtonText = styled(Text).attrs<ButtonTextProps>(
+  ({ theme, title }) => ({
+    fontSize: theme.Sizes.FONTSIZE_BUTTON_TITLE,
+    text: title,
+  }),
+)<ButtonTextProps>`
+  color: ${({ theme, buttonType, status }) =>
+    buttonType === 'outline'
+      ? theme.Colors[buttonTypes[status].color]
+      : theme.Colors.WHITE};
+  text-align: center;
+`;
+
+export const IconButton = styled(Icon).attrs(({ theme }) => ({
+  size: theme.Sizes.ICON_SIZE_BUTTON,
+}))<IconProps>`
+  margin-right: 15px;
+  color: ${({ theme, buttonType, status }) =>
+    buttonType === 'outline'
+      ? theme.Colors[buttonTypes[status].color]
+      : theme.Colors.WHITE};
+`;
