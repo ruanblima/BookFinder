7a0d8500d26154eaffa4c6bfbab1dfee749208d4
Ruan Lima <ruan.lima@dellead.com>
2021-09-27 21:54:53 -0300

    Add title in header

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -10,25 +11,43 @@ import { toogleTheme } from '~/shared/store/ducks/theme/actions';
 
 import * as S from './styles';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  enableNavigation?: boolean;
+  title?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ enableNavigation, title = '' }) => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
 
   return (
     <S.Container>
-      <S.Button onPress={() => dispatch(toogleTheme())}>
-        <S.IconColor />
-      </S.Button>
-      <S.ContainerFont>
-        <S.Button onPress={() => dispatch(decrementFontSize())}>
-          <S.DecreaseIncreaseFont>A-</S.DecreaseIncreaseFont>
+      <S.ContainerAccessibility>
+        <S.Button onPress={() => dispatch(toogleTheme())}>
+          <S.IconColor />
         </S.Button>
-        <S.Button onPress={() => dispatch(recoveryFontSize())}>
-          <S.RestoreFont>A</S.RestoreFont>
-        </S.Button>
-        <S.Button onPress={() => dispatch(incrementFontSize())}>
-          <S.DecreaseIncreaseFont>A+</S.DecreaseIncreaseFont>
-        </S.Button>
-      </S.ContainerFont>
+        <S.ContainerFont>
+          <S.Button onPress={() => dispatch(decrementFontSize())}>
+            <S.DecreaseIncreaseFont>A-</S.DecreaseIncreaseFont>
+          </S.Button>
+          <S.Button onPress={() => dispatch(recoveryFontSize())}>
+            <S.RestoreFont>A</S.RestoreFont>
+          </S.Button>
+          <S.Button onPress={() => dispatch(incrementFontSize())}>
+            <S.DecreaseIncreaseFont>A+</S.DecreaseIncreaseFont>
+          </S.Button>
+        </S.ContainerFont>
+      </S.ContainerAccessibility>
+      {enableNavigation && (
+        <S.ContainerHeader>
+          <S.ButtonLeft onPress={() => navigation.goBack()}>
+            <S.IconBack />
+          </S.ButtonLeft>
+          <S.ContainerTitle>
+            <S.Title>{title}</S.Title>
+          </S.ContainerTitle>
+        </S.ContainerHeader>
+      )}
     </S.Container>
   );
 };
--- src/shared/components/Header/styles.ts
+++ src/shared/components/Header/styles.ts
@@ -1,22 +1,44 @@
 import styled from 'styled-components/native';
 
-import { sfs, vs } from '~/shared/utils/responsibleText';
+import { s, sfs, vs } from '~/shared/utils/responsibleText';
 
 import Text from '~/shared/components/Text';
 import Icon from '~/shared/components/Icon';
 
-export const Container = styled.View`
-  height: ${vs(50)}px;
+export const Container = styled.View``;
+
+export const ContainerHeader = styled.View`
+  padding: ${vs(10)}px ${s(10)}px;
+  background: ${({ theme }) => theme.Colors.COLOR_HEADER};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContainerTitle = styled.View`
+  flex: 0.6;
+`;
+
+export const Title = styled(Text).attrs({
+  fontSize: sfs(23),
+})`
+  font-weight: bold;
+  color: ${({ theme }) => theme.Colors.TEXT_NO_CLICKABLE};
+  margin-left: 18px;
+`;
+
+export const ContainerFont = styled.View`
+  flex-direction: row;
+`;
+
+export const ContainerAccessibility = styled.View`
+  min-height: ${vs(50)}px;
   background: ${({ theme }) => theme.Colors.COLOR_APLICATION};
   flex-direction: row;
   align-items: center;
   padding-left: 24px;
   padding-right: 24px;
   justify-content: space-between;
-`;
-
-export const ContainerFont = styled.View`
-  flex-direction: row;
 `;
 
 export const RestoreFont = styled(Text).attrs({
@@ -37,9 +59,21 @@ export const DecreaseIncreaseFont = styled(Text).attrs({
 
 export const Button = styled.TouchableOpacity``;
 
+export const ButtonLeft = styled.TouchableOpacity`
+  flex: 0.3;
+  margin-left: -10px;
+`;
+
 export const IconColor = styled(Icon).attrs(({ theme }) => ({
   name: 'invert-colors',
   size: theme.Sizes.ICON_SIZE,
+}))`
+  color: ${({ theme }) => theme.Colors.TEXT_CLICKABLE};
+`;
+
+export const IconBack = styled(Icon).attrs(({ theme }) => ({
+  name: 'chevron-left',
+  size: theme.Sizes.ICON_SIZE_BACK,
 }))`
   color: ${({ theme }) => theme.Colors.TEXT_CLICKABLE};
 `;
